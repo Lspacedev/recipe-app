0f612756be46b688f3b1426814f2b66d34ebac6e
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-11-29 09:22:52 +0200

    updated index

--- app/(app)/(home)/index.tsx
+++ app/(app)/(home)/index.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, FlatList } from "react-native";
 import FAB from "@/components/FAB";
 import RecipeCard from "@/components/RecipeCard";
+import useFetch from "@/hooks/useFetch";
 type Props = {};
 const Home = (props: Props) => {
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, getFetch] = useFetch();
+  const url = process.env.EXPO_PUBLIC_API_URL ?? "";
+  useEffect(() => {
+    getFetch(url, {});
+  }, []);
   return (
     <>
       <FlatList
